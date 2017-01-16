02122203f047dbfbe6a68a8f82a499c64a598dcd
Rowan Jordaan <rowanjordaan@gmail.com>
2017-01-16 22:18:35 +0100

    Tank als pawn

---
+++ BattleTank/Source/BattleTank/MyBattleTankGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BattleTankGameModeBase.h"
+#include "MyBattleTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API AMyBattleTankGameModeBase : public ABattleTankGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
